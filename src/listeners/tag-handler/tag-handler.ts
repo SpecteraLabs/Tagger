e5fa47f39df929c4565bcc4a86139e040f8873a7
UndiedGamer <hemanthchakka2007@gmail.com>
2022-05-29 19:28:23 +0530

    feat: support options inside tags

--- src/listeners/tag-handler/tag-handler.ts
+++ src/listeners/tag-handler/tag-handler.ts
@@ -1,7 +1,15 @@
 import { ApplyOptions } from '@sapphire/decorators';
+import { isGuildBasedChannel, isGuildMember } from '@sapphire/discord.js-utilities';
 import { Listener } from '@sapphire/framework';
 import type { Interaction } from 'discord.js';
-import { InteractionTransformer } from 'tagscript-plugin-discord';
+import {
+	ChannelTransformer,
+	GuildTransformer,
+	InteractionTransformer,
+	MemberTransformer,
+	resolveCommandOptions,
+	UserTransformer
+} from 'tagscript-plugin-discord';
 
 @ApplyOptions<Listener.Options>({
 	name: 'tagHandler',
@@ -9,13 +17,20 @@ import { InteractionTransformer } from 'tagscript-plugin-discord';
 })
 export class TagHandler extends Listener {
 	public async run(interaction: Interaction) {
-		if (!interaction.guild || !interaction.isCommand()) return;
+		if (!interaction.guild || !interaction.isCommand() || !isGuildMember(interaction.member) || !isGuildBasedChannel(interaction.channel)) return;
 		const { commandName: command } = interaction;
 		const result = await this.container.db.tags.findOne(interaction.guild.id);
 		if (!result || !result.tags) return;
 		const tag = result.tags.find((t) => t.data?.name === command);
 		if (!tag) return;
-		const content = await this.container.tagscript.run(tag.content, { interaction: new InteractionTransformer(interaction) });
+		const content = await this.container.tagscript.run(tag.content, {
+			...resolveCommandOptions(interaction.options),
+			interaction: new InteractionTransformer(interaction),
+			member: new MemberTransformer(interaction.member),
+			user: new UserTransformer(interaction.user),
+			guild: new GuildTransformer(interaction.guild),
+			channel: new ChannelTransformer(interaction.channel)
+		});
 		return interaction.reply({
 			content: content.body
 		});
